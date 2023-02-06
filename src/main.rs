79513a3b2d6acf6d143c55551fe0852ba7c34151
Unknown <1650091775@qq.com>
2023-02-06 15:41:11 +0800

    add more code for benchmark

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use plonky2::iop::witness::{PartialWitness, WitnessWrite};
 use plonky2::plonk::circuit_builder::CircuitBuilder;
 use plonky2::plonk::circuit_data::CircuitConfig;
 use plonky2::plonk::config::{GenericConfig, PoseidonGoldilocksConfig};
+use plonky2::timed;
 use plonky2::util::timing::TimingTree;
 use plonky2_sha256::circuit::{array_to_bits, make_circuits};
 use sha2::{Digest, Sha256};
@@ -12,7 +13,7 @@ pub fn prove_sha256(msg: &[u8]) -> Result<()> {
     let mut hasher = Sha256::new();
     hasher.update(msg);
     let hash = hasher.finalize();
-    // println!("Hash: {:#04X}", hash);
+    println!("Hash: {:#04X}", hash);
 
     let msg_bits = array_to_bits(msg);
     let len = msg.len() * 8;
@@ -20,14 +21,25 @@ pub fn prove_sha256(msg: &[u8]) -> Result<()> {
     const D: usize = 2;
     type C = PoseidonGoldilocksConfig;
     type F = <C as GenericConfig<D>>::F;
-    let mut builder = CircuitBuilder::<F, D>::new(CircuitConfig::standard_recursion_config());
+
+    let mut config = CircuitConfig::standard_recursion_config();
+    config.num_wires = 60;
+    config.num_routed_wires = 60;
+
+    let mut builder = CircuitBuilder::<F, D>::new(config);
     let targets = make_circuits(&mut builder, len as u64);
     let mut pw = PartialWitness::new();
 
-    for i in 0..len {
-        pw.set_bool_target(targets.message[i], msg_bits[i]);
-    }
 
+    let mut timing = TimingTree::new("generate pw", Level::Info);
+    timed!(timing, "witness generation", {
+        for i in 0..len {
+            pw.set_bool_target(targets.message[i], msg_bits[i]);
+        }
+    });
+    timing.print();
+
+   
     let expected_res = array_to_bits(hash.as_slice());
     for i in 0..expected_res.len() {
         if expected_res[i] {
@@ -39,12 +51,17 @@ pub fn prove_sha256(msg: &[u8]) -> Result<()> {
 
     println!(
         "Constructing inner proof with {} gates",
-        builder.num_gates()
+        builder.num_gates(),
     );
     let data = builder.build::<C>();
     let timing = TimingTree::new("prove", Level::Debug);
     let proof = data.prove(pw).unwrap();
     timing.print();
+
+    println!(
+        "Proof size: {}",
+        proof.to_bytes().len(),
+    );
 
     let timing = TimingTree::new("verify", Level::Debug);
     let res = data.verify(proof);
@@ -60,10 +77,10 @@ fn main() -> Result<()> {
     builder.filter_level(LevelFilter::Debug);
     builder.try_init()?;
 
-    const MSG_SIZE: usize = 2828;
+    const MSG_SIZE: usize = 64;
     let mut msg = vec![0; MSG_SIZE as usize];
     for i in 0..MSG_SIZE - 1 {
-        msg[i] = i as u8;
+        msg[i] = 0 as u8;
     }
     prove_sha256(&msg)
 }
